247a6ddc0af1dec2b8e6a3c69d922797c21a8ed9
Ubaid Zahidani <er.ubaidhz@gmail.com>
2015-04-09 12:59:20 +1200

    all files with php includes

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>The Wolf of Wall Street</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style-1.css">
+	<link rel="shortcut icon" href="../favicon.ico"> 
+	<link rel="stylesheet" type="text/css" href="css/style-1.css"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        
+
+		<script src="js/modernizr.custom.70736.js"></script>
+		<noscript><link rel="stylesheet" type="text/css" href="css/noJS.css"/></noscript>
+		<!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 top-header">
+				<h1>A Martin Scorsese Picture</h1>
+				<h2>The Wolf of Wall Street</h2>
+			</div>  <!-- End of top-header -->
+		</div> <!-- End of Row -->
+		
+			<div class="row">
+					
+					<div class="col-sm-3 col-xs-12 text-center nav-bar">Trailers</div>
+					<div class="col-sm-2 col-xs-12 text-center nav-bar">Images</div>
+					<div class="col-sm-3 col-xs-12 text-center nav-bar">Sound Tracks</div>
+					<div class="col-sm-2 col-xs-12 text-center nav-bar">Cinemas</div>
+					<div class="col-sm-2 col-xs-12 text-center nav-bar">Interviews</div>
+			</div>  <!-- End of Navigation Row -->
+			<div class="row">
+				<div class=" col-xs-8 col-xs-offset-2 responsive-video"> 
+					<iframe src="https://www.youtube.com/embed/iszwuX1AK6A" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		<div class="row">
+			<div>
+				<h2>Synopis</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+			</div>  <!-- End of Synopsis -->
+
+			<div>
+				<h2>Background story</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+			</div>  <!-- End of Background Story -->
+			<div>
+				<h2>Climax</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, aspernatur quam consectetur fuga maiores esse, amet error consequatur iusto unde architecto velit ad. Labore, ad rerum voluptate neque eaque deserunt.</p>
+			</div>  <!-- End of Climax -->
+<!-- #################Gamma is the jQuery gamma gallery plugin and html markup############################ -->
+
+		<?php include 'inc/gamma.inc.php';?>
+
+<!-- #################Gamma is the jQuery gamma gallery plugin and html markup Ends here############################ -->			
+		
+
+		</div> <!-- End of Row -->
+		
+
+		<div class= "footer">
+<!-- #################Footer include goes here from inc folder under the name footer.inc.php############################ -->	
+			<?php include 'inc/footer.inc.php';?>
+<!-- #################Footer Ends here############################ -->
+		</div> <!-- Footer div Ends here -->
+	
+	</div><!--  End of Container-fluid -->
+<!-- ###########################Gamma gallery script goes here########################### -->
+		<?php include 'inc/script.inc.php';?>	
+<!-- ###########################Gamma gallery script Ends here########################### -->
+
+
+</body>
+</html>
